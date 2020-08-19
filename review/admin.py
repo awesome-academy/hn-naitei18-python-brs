594e5c3e0231bcfd956be03e8d1cb1af6586f8ea
ChiVN-sun-asstesterisk <ChiVN-sun-asterisk>
2020-08-19 14:35:28 +0700

    admin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import (
+  Book,
+  Category,
+  BookMark,)
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+  fields = ('name', )
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'image', 'desc', 'category')
+  fields = ('title', 'author', 'image', 'description', 'category')
+  search_fields = ['title']
